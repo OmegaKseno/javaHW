e0ecd12c8e54643a52d8a1548ef55ccf43da92d2
Maksim.K <omega.kseno@gmail.com>
2023-04-15 19:49:26 +0300

    test

--- hw1/hw1Dz2.java
+++ hw1/hw1Dz2.java
@@ -1,2 +1,28 @@
-package org.example.hw1;public class hw1Dz2 {
+package org.example.hw1;
+
+
+import java.util.Scanner;
+
+public class hw1Dz2 {
+    public static void main(String[] args) {
+
+        sumNumbersAfterNegatives();
+    }
+    /**
+     * sumNumbersAfterNegatives - Выводит сумму положительных чисел, после которых следует отрицательное число.
+     */
+    public static void sumNumbersAfterNegatives (){
+        Scanner scanner = new Scanner(System.in);
+        int first = scanner.nextInt();
+        int second = 0;
+        int sum = 0;
+        while(first!=0) {
+            second = scanner.nextInt();
+            if (first>0&&second<0){
+                sum+=first;
+            }
+            first=second  ;
+        }
+        System.out.println(sum);
+    }
 }
